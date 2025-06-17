d6e81c59a49dfcac448255036e8f870de9060bed
Gabriel Henrique <95993363+GabrielHenriquez@users.noreply.github.com>
2025-06-17 13:20:26 -0300

    chore: replace app.json with app.config.js
    Migrate Expo configuration from app.json to app.config.js for dynamic config support. Regenerate Android files with npx expo prebuild to align with new config.

---
+++ app.config.js
@@ -0,0 +1,46 @@
+export default {
+  expo: {
+    name: "Aluno Docs",
+    slug: "aluno-docs",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    newArchEnabled: true,
+    splash: {
+      resizeMode: "cover",
+      backgroundColor: "#1B5E20",
+      image: "./assets/splash.png",
+    },
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#ffffff",
+      },
+      edgeToEdgeEnabled: true,
+      package: "com.alunodocs.app",
+    },
+    web: {
+      favicon: "./assets/favicon.png",
+    },
+    extra: {
+      eas: {
+        projectId: "d84eb78c-61cb-4fe5-9013-416aa021d8db",
+      },
+    },
+    owner: "gabrielrick2941",
+    plugins: [
+      [
+        "expo-secure-store",
+        {
+          configureAndroidBackup: false,
+          faceIDPermission:
+            "Allow Aluno Docs to access your Face ID biometric data.",
+        },
+      ],
+    ],
+  },
+};
