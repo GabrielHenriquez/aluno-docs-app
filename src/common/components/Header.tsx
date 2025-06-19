706dab5387962a405b1ee49991d9e0e16dae75db
Gabriel Henrique <95993363+GabrielHenriquez@users.noreply.github.com>
2025-06-19 11:22:52 -0300

    feat: add secondary header button

--- src/common/components/Header.tsx
+++ src/common/components/Header.tsx
@@ -1,32 +1,46 @@
 import StatusBar from "./StatusBar";
 import Text from "./Text";
-import { IColors } from "@styles/model";
-import { ChevronLeft } from "lucide-react-native";
-import { colors } from "@styles/colors";
+import { ChevronLeft, Rows } from "lucide-react-native";
+import { colors, IColors } from "@styles/colors";
+import { ReactNode } from "react";
 import * as RN from "react-native";
 
 const Header = ({
   color = "black",
   bgColor = "background",
+  hasButtonSecondary,
+  onPressSecondary,
+  icon,
   title,
   onPressBack,
 }: {
   color?: IColors;
   bgColor?: IColors;
   title?: string;
+  icon?: ReactNode;
+  hasButtonSecondary?: boolean;
   onPressBack?: VoidFunction;
+  onPressSecondary?: VoidFunction;
 }) => {
   return (
     <>
       <StatusBar backgroundColor={bgColor} />
       <RN.View style={[Styles.Container, { backgroundColor: colors[bgColor] }]}>
-        <RN.TouchableOpacity style={Styles.BackButton} onPress={onPressBack}>
-          <ChevronLeft size={45} color={colors[color]} />
-        </RN.TouchableOpacity>
-        {title && (
-          <Text className="font-interBold text-black" size={26}>
-            {title}
-          </Text>
+        <RN.View className="flex-row gap-1 items-center">
+          <RN.TouchableOpacity style={Styles.BackButton} onPress={onPressBack}>
+            <ChevronLeft size={45} color={colors[color]} />
+          </RN.TouchableOpacity>
+          {title && (
+            <Text className="font-interBold text-black" size={26}>
+              {title}
+            </Text>
+          )}
+        </RN.View>
+
+        {hasButtonSecondary && (
+          <RN.TouchableOpacity onPress={onPressSecondary}>
+            {icon}
+          </RN.TouchableOpacity>
         )}
       </RN.View>
     </>
@@ -35,11 +49,12 @@ const Header = ({
 
 const Styles = RN.StyleSheet.create({
   Container: {
-    gap: 4,
     paddingTop: 8,
     paddingBottom: 4,
-    flexDirection: "row",
+    paddingRight: 14,
     alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
   BackButton: {},
 });
