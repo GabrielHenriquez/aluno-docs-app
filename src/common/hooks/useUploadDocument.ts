cba504dbf96ed211c5a6e7f290007d2c9bdd7f29
Gabriel Henrique <95993363+GabrielHenriquez@users.noreply.github.com>
2025-06-19 11:36:25 -0300

    feat: type and distribute method onSuccess in UploadDocument

--- src/common/hooks/useUploadDocument.ts
+++ src/common/hooks/useUploadDocument.ts
@@ -1,16 +1,28 @@
 import documents from "@services/documents";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { StorageKeys } from "@utils/storageKeys";
+import { IUploadedDocument, IUploadPayload } from "@models/document";
 
-export const useUploadDocument = () => {
+type UploadDocumentOptions = {
+  onSuccess?: (
+    data: IUploadedDocument,
+    variables: IUploadPayload,
+    context: unknown
+  ) => void;
+};
+
+export const useUploadDocument = (options?: UploadDocumentOptions) => {
   const queryClient = useQueryClient();
 
-  return useMutation({
+  return useMutation<IUploadedDocument, unknown, IUploadPayload>({
     mutationFn: documents.uploadDocument,
-    onSuccess: () => {
+    onSuccess: (data, variables, context) => {
       queryClient.invalidateQueries({
         queryKey: [StorageKeys.UPLOADED_DOCUMENTS],
       });
+
+      options?.onSuccess?.(data, variables, context);
     },
+    
   });
 };
